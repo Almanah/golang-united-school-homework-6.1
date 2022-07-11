3f342aa74467843b33522da9a5eddce7370e6106
Denis Savchyshkin <Denis_Savchyshkin@epam.com>
2022-07-11 15:32:56 +0300

    initial solution

--- box.go
+++ box.go
@@ -1,5 +1,15 @@
 package golang_united_school_homework
 
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	errorShapeByIndexNotExists = errors.New("shape by index does not exist")
+	errorOutOfRange            = errors.New("index is out of range")
+	errorNoCircles             = errors.New("no circles in the box")
+)
 
 // box contains list of shapes and able to perform operations on them
 type box struct {
@@ -17,45 +27,93 @@ func NewBox(shapesCapacity int) *box {
 // AddShape adds shape to the box
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
-	panic("implement me")
+	if len(b.shapes) >= b.shapesCapacity {
+		return fmt.Errorf("got error: %w", errorOutOfRange)
+	}
+	b.shapes = append(b.shapes, shape)
+	return nil
 }
 
 // GetByIndex allows getting shape by index
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
-	panic("implement me")
+	if i >= b.shapesCapacity {
+		return nil, fmt.Errorf("got error: %w", errorOutOfRange)
+	}
+	shapeByIdx := b.shapes[i]
 
+	if shapeByIdx == nil {
+		return nil, fmt.Errorf("got error: %w", errorShapeByIndexNotExists)
+	}
+	return shapeByIdx, nil
 }
 
 // ExtractByIndex allows getting shape by index and removes this shape from the list.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
-	panic("implement me")
+	if i >= b.shapesCapacity {
+		return nil, fmt.Errorf("got error: %w", errorOutOfRange)
+	}
+	shapeByIdx := b.shapes[i]
 
+	if shapeByIdx == nil {
+		return nil, fmt.Errorf("got error: %w", errorShapeByIndexNotExists)
+	}
+	b.shapes = append(b.shapes[:i], b.shapes[i+1:]...)
+	return shapeByIdx, nil
 }
 
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
-	panic("implement me")
+	if i >= b.shapesCapacity {
+		return nil, fmt.Errorf("got error: %w", errorOutOfRange)
+	}
+	shapeByIdx := b.shapes[i]
 
+	if shapeByIdx == nil {
+		return nil, fmt.Errorf("got error: %w", errorShapeByIndexNotExists)
+	}
+	b.shapes[i] = shape
+	return shapeByIdx, nil
 }
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
 func (b *box) SumPerimeter() float64 {
-	panic("implement me")
+	var sum float64 = 0
 
+	for _, shape := range b.shapes {
+		sum += shape.CalcPerimeter()
+	}
+	return sum
 }
 
 // SumArea provides sum area of all shapes in the list.
 func (b *box) SumArea() float64 {
-	panic("implement me")
+	var sum float64 = 0
 
+	for _, shape := range b.shapes {
+		sum += shape.CalcArea()
+	}
+	return sum
 }
 
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
-	panic("implement me")
+	var newShapes []Shape
+	wasRemoved := false
 
+	for _, shape := range b.shapes {
+		if _, isCircle := shape.(*Circle); !isCircle {
+			wasRemoved = true
+			newShapes = append(newShapes, shape)
+		}
+	}
+
+	if !wasRemoved {
+		return fmt.Errorf("got error: %w", errorNoCircles)
+	}
+	b.shapes = newShapes
+	return nil
 }
